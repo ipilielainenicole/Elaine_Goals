e3e910d2505564cbad20bd8a3bd0976ea7ad575a
unknown <elainenicole444@gmail.com>
2025-08-27 09:46:54 +0800

    Firebase Config

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA6rIX2zQIdjj4ftkKfxqz2YoEm_YHTqPE",
+  authDomain: "elaine-goals.firebaseapp.com",
+  projectId: "elaine-goals",
+  storageBucket: "elaine-goals.firebasestorage.app",
+  messagingSenderId: "297968928346",
+  appId: "1:297968928346:web:407756f666b0c61817ba63"
+};
+
+ 
+const app = initializeApp(firebaseConfig);
